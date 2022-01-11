39fa0c761e69a297b20cb19b2b4d0475961efa92
superNOVA <jinglu_cui@163.com>
2022-01-11 13:14:52 -0800

    docs: created docstrings for package function

--- src/date_converter/date_converter.py
+++ src/date_converter/date_converter.py
@@ -3,6 +3,29 @@ from datetime import date
 
 
 def date_converter(dt, f='%Y-%m-%d'):
+    """Convert datenum to date string, or date string to datenum
+
+    Parameters
+    ----------
+    dt : str or int
+        Date number or date string to be converted.
+    f : str, optional
+        Date string formate.
+
+    Returns
+    -------
+    int or str
+        String if input is int, or int 'if input is string.
+
+
+    Examples
+    --------
+    >>> date_converter('2020-01-01')
+    >>> date_converter(737425)
+    """
+
+
+
     if isinstance(dt, str):
         return date.toordinal(datetime.strptime(dt, f))
     elif isinstance(dt, int):

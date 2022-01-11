8b613f8a8391464dbc37487d34574768fb9e5051
superNOVA <jinglu_cui@163.com>
2022-01-11 12:39:09 -0800

    test: add unit test for date_converter

--- tests/test_date_converter.py
+++ tests/test_date_converter.py
@@ -1,1 +1,8 @@
-from date_converter import date_converter
+from date_converter.date_converter import date_converter
+
+
+def test_date_converter():
+    """Test date_converter"""
+    dt1 = '2020-01-01'
+    dt_num = date_converter(dt1)
+    assert dt1 == date_converter(dt_num), 'Converter failed!'
